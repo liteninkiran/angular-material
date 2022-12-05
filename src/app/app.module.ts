3e0cdc5b46a44d33e6bbbc5a3ee626411ffa6d27
Kiran Anand <kanand@prevalent.net>
2022-12-05 13:48:55 +0000

    Create module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
     declarations: [
@@ -13,9 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCheckboxModule,
+        MaterialModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
